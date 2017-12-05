5df9d14eb9ed8f7bd1dd3a9583531f68ef6d070d
Oran C <oranbusiness@gmail.com>
2017-12-05 12:39:05 -0800

    Feature Get terminal ouput
    saves terminal output to a variable

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import os
+import subprocess
 # Termux Cell Heatmapper
 # by wisemonkey
 # 20171205
 # main.py program for outputing the location and signal strangth to a file
+
+get_signal_strength_command = "dir ."
+signal_strength_json = os.popen(get_signal_strength_command).read()
+
+print(signal_strength_json)
