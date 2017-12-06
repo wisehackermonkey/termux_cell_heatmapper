6a40f8b9a2b5a550339c4de93e8e539d94a9ffdc
Collins Oran <ocollins@santarosa.edu>
2017-12-06 11:36:30 -0800

    added file for ouput

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ import subprocess
 # 20171205
 # main.py program for outputing the location and signal strangth to a file
 
+file = open("ouput.txt")
+
 get_signal_strength_command = "dir ."
 #https://stackoverflow.com/questions/20140137/passing-variables-to-subprocess-popen
 signal_strength_json = os.popen(get_signal_strength_command).read()
 
 print(signal_strength_json)
+
+file.write(signal_strength_json)
+
+file.close()
+
+
