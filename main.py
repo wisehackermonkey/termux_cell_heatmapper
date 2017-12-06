a53888ad6d0c07c273fc76c518951a3a87386ee5
Collins Oran <ocollins@santarosa.edu>
2017-12-06 13:47:29 -0800

    updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import csv
+import json 
 # Termux Cell Heatmapper
 # by wisemonkey
 # 20171205
@@ -12,6 +13,10 @@ signal_strength_json = os.popen(get_signal_strength_command).read()
 
 print(signal_strength_json)
 
+#http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
+#https://stackoverflow.com/questions/11151719/python-writerows-trouble
 with open('data.csv', 'w') as file:
 	csv_file = csv.writer(file)
-	csv_file.writerow(["asdf","asd"]) #todo get json to here in [loc, dbm]
+	test = json.loads('"asdf","asd"')
+	print(test)
+	csv_file.writerow(csv) #todo get json to here in [loc, dbm]
