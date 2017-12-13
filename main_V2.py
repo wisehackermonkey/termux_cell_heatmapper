e183031005fe29c667235c29c8e9a3662c644be2
wisehackermonkey <oranbusiness@gmail.com>
2017-12-13 14:31:45 -0800

    rewrite
    added json
    with and more

---
+++ main_V2.py
@@ -0,0 +1,25 @@
+﻿import os
+import json
+import csv
+#todo https://docs.python.org/3.5/library/datetime.html#module-datetime
+
+shell_command_get_gps_location = "type json.json.txt"#"termux-location -p gps -r once"
+shell_command_get_cell_signstrength = "type json.json.txt"#"termux-telephony-cellinfo"
+
+
+
+
+with open('heatmap.csv', 'a+') as csvfile:
+	heatmap = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+	location = ''
+	signal = ''
+	with os.popen(shell_command_get_gps_location) as location_json:
+		location = json.loads(location_json.read())
+		print(location)
+
+	with os.popen(shell_command_get_cell_signstrength) as signal_json:
+		signal = json.loads(signal_json.read())
+		print(signal)
+
+	heatmap.writerow([location["id"]] + [signal["id"]])
